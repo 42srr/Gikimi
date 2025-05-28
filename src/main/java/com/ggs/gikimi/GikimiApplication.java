0a5c35f263ecc563f725d46805830f1c903d2756
joonwan <111417356+joonwan@users.noreply.github.com>
2025-05-28 22:42:43 +0900

    feat: init project

---
+++ src/main/java/com/ggs/gikimi/GikimiApplication.java
@@ -0,0 +1,13 @@
+package com.ggs.gikimi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GikimiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GikimiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ggs/gikimi/GikimiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggs.gikimi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GikimiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
